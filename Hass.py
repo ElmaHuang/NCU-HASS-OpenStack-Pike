37f39edb2dd76c6bcb52a2cf27b5f16b64b7619f
ElmaHuang <huanghuei0206@gmail.com>
2017-10-02 23:59:29 +0800

    10/2

--- Hass.py
+++ Hass.py
@@ -18,23 +18,47 @@ from ClusterManager import ClusterManager
 from IPMINodeOperator import Operator
 #from IPMIModule import IPMIManager
 
+# Declare the configure file here. if you want to change configure file name, please modify : hass.conf.
+'''
+config = ConfigParser.RawConfigParser()
+config.read('hass.conf')
+
+# Set log file here. if you want to change log format, please modify : %(asctime)s [%(levelname)s] : %(message)s.
+log_level = logging.getLevelName(config.get("log", "level"))
+logFilename=config.get("log", "location")
+dir = os.path.dirname(logFilename)
+if not os.path.exists(dir):
+    os.makedirs(dir)
+logging.basicConfig(filename=logFilename,level=log_level, format="%(asctime)s [%(levelname)s] : %(message)s")
+'''
+# recovery = None
+# if len(sys.argv) == 2:
+#     print "System Test = ", sys.argv[1]
+#     recovery = Recovery(system_test = sys.argv[1])
+# else:
+# # Declare Recovery class. You need to ensure that there is only one object. So I declare it as global variable.
+#     recovery = Recovery()
+
+#ipmi_manager = IPMIManager()
+#ClusterManager.init()
+
 class RequestHandler(SimpleXMLRPCRequestHandler):
-    #   Handle RPC request from remote user, and suport access authenticate.
-    #
-    #   HTTP basic access authentication are encoded with Base64 in transit, but not
-    #   encrypted or hashed in any way. Authentication field contain authentication
-    #   method, username and password combined into a string. If request not contain
-    #   authentication header or contain not correct username and password, it will
-    #   return 401 error code. Otherwise, handle request and return response.
+#   Handle RPC request from remote user, and suport access authenticate. 
+#
+#   HTTP basic access authentication are encoded with Base64 in transit, but not
+#   encrypted or hashed in any way. Authentication field contain authentication
+#   method, username and password combined into a string. If request not contain
+#   authentication header or contain not correct username and password, it will
+#   return 401 error code. Otherwise, handle request and return response.
 
     def __init__(self, request, client_address, server):
-        # initialize rpc server and get client ip address. call parent initial method.
+    # initialize rpc server and get client ip address. call parent initial method.
         rpc_paths = ('/RPC2',)
         self.clientip = client_address[0]
         SimpleXMLRPCRequestHandler.__init__(self, request, client_address, server)
-
+        
     def authenticate(self, headers):
-        # split authentication header, decode with Base64 and check username and password
+    # split authentication header, decode with Base64 and check username and password
         auth = headers.get('Authorization')
         try:
             (basic, encoded) = headers.get('Authorization').split(' ')
@@ -50,7 +74,7 @@ class RequestHandler(SimpleXMLRPCRequestHandler):
             (username, password) = decodedString.split(':')
             config = ConfigParser.RawConfigParser()
             config.read('hass.conf')
-            if username == config.get("rpc", "rpc_username") and password == config.get("rpc", "rpc_password"):
+            if username == config.get("rpc", "rpc_username") and password == config.get("rpc", "rpc_password"):                
                 print "Login"
                 return True
             else:
@@ -58,10 +82,10 @@ class RequestHandler(SimpleXMLRPCRequestHandler):
                 return False
 
     def parse_request(self):
-        # parser request, get authentication header and send to authenticate().
+    # parser request, get authentication header and send to authenticate().
         if SimpleXMLRPCRequestHandler.parse_request(self):
             if self.authenticate(self.headers):
-                # logging.info("Hass RequestHandler - Authentication success, request from %s", self.clientip)
+                #logging.info("Hass RequestHandler - Authentication success, request from %s", self.clientip)
                 return True
             else:
                 self.send_error(401, 'Authentication failed')
@@ -69,23 +93,24 @@ class RequestHandler(SimpleXMLRPCRequestHandler):
         else:
             logging.info("Hass RequestHandler - Authentication failed, request from %s", self.clientip)
             return False
-
+        
 
 class Hass (object):
-
+#   The SimpleRPCServer class
+#   Declare method here, and client can call it directly.
+#   All of methods just process return data from recovery module
     def __init__(self):
-        self.config = ConfigParser.RawConfigParser()
-        self.config.read('hass.conf')
+        #self.config = ConfigParser.RawConfigParser()
+        #self.config.read('hass.conf')
 
-        log_level = logging.getLevelName(self.config.get("log", "level"))
-        logFilename = self.config.get("log", "location")
-        dir = os.path.dirname(logFilename)
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-        logging.basicConfig(filename=logFilename, level=log_level, format="%(asctime)s [%(levelname)s] : %(message)s")
-        #ipmi_manager = IPMIManager()
-        self.ClusterManager = ClusterManager()
-
+        #log_level = logging.getLevelName(self.config.get("log", "level"))
+        #logFilename = self.config.get("log", "location")
+        #dir = os.path.dirname(logFilename)
+        #if not os.path.exists(dir):
+         #   os.makedirs(dir)
+        #logging.basicConfig(filename=logFilename, level=log_level, format="%(asctime)s [%(levelname)s] : %(message)s")
+        # ipmi_manager = IPMIManager()
+        ClusterManager.init()
         self.Operator = Operator()
         self.Recovery = RecoveryManager()
 
@@ -94,61 +119,67 @@ class Hass (object):
         return "auth success"
         
     def createCluster(self, name, nodeList=[]):
-        result = self.ClusterManager.createCluster(name)
+        result = ClusterManager.createCluster(name)
         if result["code"] == "0":
             if nodeList != []:
-                addNode_result = self.ClusterManager.addNode(result["clusterId"], nodeList)
+                addNode_result = ClusterManager.addNode(result["clusterId"], nodeList)
             else :
-                addNode_result = {"code":"0", "clusterId":result["clusterId"], "message":"not add any node."}
+                addNode_result = {"code":0, "clusterId":result["clusterId"], "message":"not add any node."}
                     
             if addNode_result["code"] == "0":
                 return "0;Create HA cluster and add computing node success, cluster uuid is %s , %s" % (result["clusterId"] , addNode_result["message"])
             else:
-                return "1;The cluster is created.(uuid = "+result["clusterId"]+") But,"+ addNode_result["message"]
+                return "0;The cluster is created.(uuid = "+result["clusterId"]+") But,"+ addNode_result["message"]
         else:
             return result["code"]+";"+result["message"]
 
     def deleteCluster(self, uuid, test=False):
-        result = self.ClusterManager.deleteCluster(uuid)
+        result = ClusterManager.deleteCluster(uuid)
         return result["code"]+";"+result["message"]
     
     def listCluster(self):
-        result = self.ClusterManager.listAllCluster()
+        result = ClusterManager.listCluster()
         return result
     
     def addNode(self, clusterId, nodeList, test=False):
-        result = self.ClusterManager.addNode(clusterId, nodeList)
+        result = ClusterManager.addNode(clusterId, nodeList)               
         return result["code"]+";"+result["message"]
 
-    def deleteNode(self, clusterId, nodename, test=False):
-        result = self.ClusterManager.deleteNode(clusterId, nodename)
+    def deleteNode(self, cluster_id, node_id, test=False):
+        result = ClusterManager.deleteNode(cluster_id, node_id)
         return result["code"]+";"+result["message"]
         
     def listNode(self, clusterId) :
-        result = self.ClusterManager.listAllNode(clusterId)
-        if result["code"]== "0":
-            return result["code"]+";"+result["nodeList"]
-        else:
-            return result["code"]+";"+result["message"]
+        result = ClusterManager.listNode(clusterId)
+        return result
+
+    def addInstance(self, clusterId, instanceId):
+        result = ClusterManager.addInstance(clusterId, instanceId)
+        return result["code"] + ";" + result["message"]
+
+    def deleteInstance(self, clusterId, instanceId):
+        result = ClusterManager.deleteInstance(clusterId, instanceId)
+        return result["code"] + ";" + result["message"]
+
+    def listInstance(self, clusterId):
+        result = ClusterManager.listInstance(clusterId)
+        return result
             
     def startNode(self, nodeName):
-        result = self.Operator.Start_Node(nodeName)
+        result = self.Operator.startNode(nodeName)
         return result["code"] + ";" + result["message"]
 
     def shutOffNode(self, nodeName):
-        result = self.Operator.Shut_Off_Node(nodeName)
+        result = self.Operator.shutOffNode(nodeName)
         return result["code"] + ";" + result["message"]
 
     def rebootNode(self, nodeName):
-        result = self.Operator.Reboot_Node(nodeName)
+        result = self.Operator.rebootNode(nodeName)
         return result["code"] + ";" + result["message"]
 
     def getAllInfoOfNode(self, nodeName):
-        result = self.Operator.getAllInfoOfNode(nodeName)
-        if result["code"] == "0":
-            return result["code"]+";"+result["info"]
-        else:
-            return result["code"] + ";" + result["message"]
+        result = self.Operator.getTempInfoByNode(nodeName)
+        return result["code"] + ";" + result["info"]
 
     def getNodeInfoByType(self, nodeName, sensorType):
         result = self.Operator.getNodeInfoByType(nodeName, sensorType)
@@ -156,22 +187,6 @@ class Hass (object):
             return result["code"], result["info"]
         else: 
             return result["code"] + ";" + result["message"]
-
-    def addInstance(self, clusterId, instanceId):
-        result = self.ClusterManager.addInstance(clusterId, instanceId)
-        return result["code"]+";"+result["message"]
-    
-    def deleteInstance(self, clusterId, instanceId):
-        result = self.ClusterManager.deleteInstance(clusterId, instanceId)
-        return result["code"]+";"+result["message"]
-    
-    def listInstance(self, clusterId) :
-        result = self.ClusterManager.listInstance(clusterId)
-        if result["code"]== "0":
-            return result["code"]+";"+result["instanceList"]
-        else:
-            return result["code"]+";"+result["message"]
-            
     def recoveryVM(self, clusterId, nodeName):
         result = self.Recovery.recoveryVM(clusterId, nodeName)
 
@@ -206,14 +221,22 @@ class Hass (object):
         result = self.Recovery.recoveryPowerOff(clusterId, nodeName)
         return result
 
-
-
 def main():
-    hass = Hass()
-    server = SimpleXMLRPCServer(('',int(hass.config.get("rpc", "rpc_bind_port"))), requestHandler=RequestHandler, allow_none = True, logRequests=False)
+    config = ConfigParser.RawConfigParser()
+    config.read('hass.conf')
+
+    log_level = logging.getLevelName(config.get("log", "level"))
+    logFilename = config.get("log", "location")
+    dir = os.path.dirname(logFilename)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    logging.basicConfig(filename=logFilename, level=log_level, format="%(asctime)s [%(levelname)s] : %(message)s")
+    # ipmi_manager = IPMIManager()
+
+    server = SimpleXMLRPCServer(('',int(config.get("rpc", "rpc_bind_port"))), requestHandler=RequestHandler, allow_none = True, logRequests=False)
     server.register_introspection_functions()
     server.register_multicall_functions()
-    server.register_instance(hass, allow_dotted_names=True)
+    server.register_instance(Hass(), allow_dotted_names=True)
 
     print "HASS Server ready"
     try:
