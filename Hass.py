9c3ed3a3936e8bd22d9fc4b3ca59c07ee1482fc2
ElmaHuang <huanghuei0206@gmail.com>
2017-10-06 15:09:16 +0800

    rename

--- Hass.py
+++ Hass.py
@@ -13,10 +13,13 @@ import logging
 import os
 import sys
 
+from RecoveryManager import RecoveryManager
 from ClusterManager import ClusterManager
-from IPMIModule import IPMIManager
+from IPMINodeOperator import Operator
+#from IPMIModule import IPMIManager
 
 # Declare the configure file here. if you want to change configure file name, please modify : hass.conf.
+'''
 config = ConfigParser.RawConfigParser()
 config.read('hass.conf')
 
@@ -27,7 +30,7 @@ dir = os.path.dirname(logFilename)
 if not os.path.exists(dir):
     os.makedirs(dir)
 logging.basicConfig(filename=logFilename,level=log_level, format="%(asctime)s [%(levelname)s] : %(message)s")
-
+'''
 # recovery = None
 # if len(sys.argv) == 2:
 #     print "System Test = ", sys.argv[1]
@@ -36,8 +39,6 @@ logging.basicConfig(filename=logFilename,level=log_level, format="%(asctime)s [%
 # # Declare Recovery class. You need to ensure that there is only one object. So I declare it as global variable.
 #     recovery = Recovery()
 
-ipmi_manager = IPMIManager()
-ClusterManager.init()
 
 class RequestHandler(SimpleXMLRPCRequestHandler):
 #   Handle RPC request from remote user, and suport access authenticate. 
@@ -96,6 +97,20 @@ class Hass (object):
 #   The SimpleRPCServer class
 #   Declare method here, and client can call it directly.
 #   All of methods just process return data from recovery module
+    def __init__(self):
+        #self.config = ConfigParser.RawConfigParser()
+        #self.config.read('hass.conf')
+
+        #log_level = logging.getLevelName(self.config.get("log", "level"))
+        #logFilename = self.config.get("log", "location")
+        #dir = os.path.dirname(logFilename)
+        #if not os.path.exists(dir):
+         #   os.makedirs(dir)
+        #logging.basicConfig(filename=logFilename, level=log_level, format="%(asctime)s [%(levelname)s] : %(message)s")
+        # ipmi_manager = IPMIManager()
+        ClusterManager.init()
+        self.Operator = Operator()
+        self.Recovery = RecoveryManager()
 
     def test_auth_response(self):
     #Unit tester call this function to get successful message if authenticate success.
@@ -107,53 +122,54 @@ class Hass (object):
             if nodeList != []:
                 addNode_result = ClusterManager.addNode(result["clusterId"], nodeList)
             else :
-                addNode_result = {"code":0, "clusterId":result["clusterId"], "message":"not add any node."}
-                    
+
+                addNode_result = {"code":"0", "clusterId":result["clusterId"], "message":"not add any node."}
+
             if addNode_result["code"] == "0":
                 return "0;Create HA cluster and add computing node success, cluster uuid is %s , %s" % (result["clusterId"] , addNode_result["message"])
             else:
-                return "0;The cluster is created.(uuid = "+result["clusterId"]+") But,"+ addNode_result["message"]
+                return "1;The cluster is created.(uuid = "+result["clusterId"]+") But,"+ addNode_result["message"]
         else:
             return result["code"]+";"+result["message"]
 
-    def deleteCluster(self, uuid, test=False):
+    def deleteCluster(self, uuid,write_DB = True):
         result = ClusterManager.deleteCluster(uuid)
         return result["code"]+";"+result["message"]
     
     def listCluster(self):
         result = ClusterManager.listCluster()
         return result
-    
-    def addNode(self, clusterId, nodeList, test=False):
+
+    def addNode(self, clusterId, nodeList, writeDB=True):
         result = ClusterManager.addNode(clusterId, nodeList)               
         return result["code"]+";"+result["message"]
 
-    def deleteNode(self, cluster_id, node_id, test=False):
+    def deleteNode(self, cluster_id, node_id, writeDB=True):
         result = ClusterManager.deleteNode(cluster_id, node_id)
         return result["code"]+";"+result["message"]
         
     def listNode(self, clusterId) :
         result = ClusterManager.listNode(clusterId)
         return result
-            
+
     def startNode(self, nodeName):
-        result = ipmi_manager.startNode(nodeName)
+        result = self.Operator.startNode(nodeName)
         return result["code"] + ";" + result["message"]
 
     def shutOffNode(self, nodeName):
-        result = ipmi_manager.shutOffNode(nodeName)
+        result = self.Operator.shutOffNode(nodeName)
         return result["code"] + ";" + result["message"]
 
     def rebootNode(self, nodeName):
-        result = ipmi_manager.rebootNode(nodeName)
+        result = self.Operator.rebootNode(nodeName)
         return result["code"] + ";" + result["message"]
 
     def getAllInfoOfNode(self, nodeName):
-        result = ipmi_manager.getTempInfoByNode(nodeName)
+        result = self.Operator.getTempInfoByNode(nodeName)
         return result["code"] + ";" + result["info"]
 
     def getNodeInfoByType(self, nodeName, sensorType):
-        result = ipmi_manager.getNodeInfoByType(nodeName, sensorType)
+        result = self.Operator.getNodeInfoByType(nodeName, sensorType)
         if result["code"] == "0":
             return result["code"], result["info"]
         else: 
@@ -170,43 +186,53 @@ class Hass (object):
     def listInstance(self, clusterId) :
         result = ClusterManager.listInstance(clusterId)
         return result
-            
-    def recoveryVM(self, clusterId, nodeName):
-        result = recovery.recoveryVM(clusterId, nodeName)
 
-    def removeNodeFromCluster(self, clusterId, nodeName):
-        result = recovery.remove_node_from_cluster(clusterId, nodeName)
+    def recoveryVM(self, clusterId, nodeName):
+        result = self.Recovery.recoveryVM(clusterId, nodeName)
+
+    #def removeNodeFromCluster(self, clusterId, nodeName):
+       # result = self.Recovery.remove_node_from_cluster(clusterId, nodeName)
 
     def recoveryByShutOffNode(self, clusterId, nodeName, option):
-        result = recovery.recovery_by_shut_Off_Node(clusterId, nodeName)
+        result = self.Recovery.recoveryByShutOffNode(clusterId, nodeName)
         return result
 
     def recoveryServiceFailure(self, clusterId, nodeName, service_list):
-        result = recovery.recovery_service_failure(clusterId, nodeName, service_list)
+        result = self.Recovery.recoveryServiceFailure(clusterId, nodeName, service_list)
         return result
 
     def recoveryIPMIDaemonFailure(self, clusterId, nodeName, option):
-        result = recovery.recovery_ipmi_daemon_failure(nodeName)
+        result = self.Recovery.recoveryIpmiDaemonFailure(nodeName)
         return result
 
     def recoveryWatchdogDaemonFailure(self, clusterId, nodeName, option):
-        result = recovery.recovery_watchdog_daemon_failure(nodeName)
+        result = self.Recovery.recoveryWatchdogDaemonFailure(nodeName)
         return result
 
     def recoveryOSHanged(self, clusterId, nodeName, option):
-        result = recovery.recovery_os_hanged(clusterId, nodeName)
+        result = self.Recovery.recoveryOsHanged(clusterId, nodeName)
         return result
 
     def recoveryNetworkFailure(self, clusterId, nodeName, option):
-        result = recovery.recovery_network_failure(clusterId, nodeName)
+        result = self.Recovery.recoveryNetworkFailure(clusterId, nodeName)
         return result
 
     def recoveryPowerOff(self, clusterId, nodeName, option):
-        result = recovery.recovery_power_off(clusterId, nodeName)
+        result = self.Recovery.recoveryPowerOff(clusterId, nodeName)
         return result
 
 def main():
-    
+    config = ConfigParser.RawConfigParser()
+    config.read('hass.conf')
+
+    log_level = logging.getLevelName(config.get("log", "level"))
+    logFilename = config.get("log", "location")
+    dir = os.path.dirname(logFilename)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    logging.basicConfig(filename=logFilename, level=log_level, format="%(asctime)s [%(levelname)s] : %(message)s")
+    # ipmi_manager = IPMIManager()
+
     server = SimpleXMLRPCServer(('',int(config.get("rpc", "rpc_bind_port"))), requestHandler=RequestHandler, allow_none = True, logRequests=False)
     server.register_introspection_functions()
     server.register_multicall_functions()
