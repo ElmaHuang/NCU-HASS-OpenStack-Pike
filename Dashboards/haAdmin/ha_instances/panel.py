0fb5c88e49e4abda0e8d88595e437be1670c362a
ElmaHuang <huanghuei0206@gmail.com>
2017-12-16 08:05:13 +0800

    fix gui

--- Dashboards/haAdmin/ha_instances/panel.py
+++ Dashboards/haAdmin/ha_instances/panel.py
@@ -6,7 +6,7 @@ from openstack_dashboard.dashboards.haAdmin import dashboard
 
 
 class HA_Instances(horizon.Panel):
-    name = _("HA Inatances")
+    name = _("HA Instances")
     slug = "ha_instances"
     #permissions = ('openstack.services.compute')
 
