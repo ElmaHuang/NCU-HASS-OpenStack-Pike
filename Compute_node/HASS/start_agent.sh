4431c944f6d5425ced1e2eb2ad4f18220df7d6de
ElmaHuang <huanghuei0206@gmail.com>
2018-02-05 17:48:33 +0800

    set detectionagent daemon service

---
+++ Compute_node/HASS/start_agent.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+cd /home/compute2/Desktop/HASS/Compute_node/HASS
+python DetectionAgent.py
+
+#chmod u+x start_agent.sh
--- Compute_node/UnitTest/os_hang.sh
+++ Compute_node/UnitTest/os_hang.sh
@@ -2,3 +2,5 @@
 
 sudo strace -p 1 &
 sudo kill -9 1
+
+#chmod +x os_hang.sh
